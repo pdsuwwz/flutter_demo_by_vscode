dc7d36c2bc6c62992a946c331b4fb3961119694a
pdsuwwz <pdsu.wwz@foxmail.com>
2023-01-12 18:53:45 +0800

    feat: init repo

---
+++ lib/i18n/index.dart
@@ -0,0 +1,1 @@
+library i18n;
---
+++ lib/modules/module_1/components/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({super.key});
+
+  @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [Text(' Hello World！'), Text(' Hello World！')]),
+    );
+  }
+}
